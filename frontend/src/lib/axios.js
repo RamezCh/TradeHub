80a126dc0da27b252985a0ff4d6758fe69c83a11
Ramez Chreide <ramiz_chreide@outlook.com>
2025-01-17 18:40:49 +0100

    Prepare Project for Hosting

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // backend api link
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "/api", // backend api link
   withCredentials: true, // Send cookies with every single request
 });
